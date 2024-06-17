2ab45ebb7f30c55eaaedfda35ab32f3e248e3d4b
anisha-jadhav <jadhavanisha3033@gmail.com>
2024-06-17 17:25:50 +0530

    Added time-trvel in game.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,46 +1,44 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Square } from  './Square'
 
-
-export const Board = () => {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsnext] = useState(true);
-
+export const Board = ({xIsNext, squares, onPlay}) => {
   function handleClick(i){
-    if(squares[i] || calculateWinner(squares)){
+    if(calculateWinner(squares) || squares[i]){
       return;
     }
 
     const nextSquares = squares.slice();
     xIsNext === true ? nextSquares[i] = 'X': nextSquares[i] = 'O';
 
-    setSquares(nextSquares);
-    setXIsnext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   function calculateWinner(squares){
-    const lines = [
-      [0,1,2], [3,4,5], [6,7,8],
-      [0,3,6], [1,4,7], [3,6,9],
-      [0,4,8], [2,4,6]
-    ];
-
-    for( let i=0; i<lines.length; i++){
-      const [a,b,c] = lines[i];
-      if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
-        return squares[a];
-      }
-    }
+   const lines = [
+     [0,1,2], [3,4,5], [6,7,8],
+     [0,3,6], [1,4,7], [2,5,8],
+     [0,4,8], [2,4,6]
+   ];
+    
+   for( let i=0; i<lines.length; i++){
+     const [a,b,c] = lines[i];
+     if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+       return squares[a];
+     }
+   }
+   return null;
   }
+  
+  
 
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner){
-    status = "Winner : " + winner; 
-  } else {
-    status = "Next Player : " + (xIsNext ? "X":"O");
-  }
+ const winner = calculateWinner(squares);
+ let status;
+ if(winner){
+   status = "Winner : " + winner; 
+ } else {
+   status = "Next Player : " + (xIsNext ? "X":"O");
+ }
+  
   return (
     <>
     <div className='status'>{status}</div>
@@ -62,3 +60,4 @@ export const Board = () => {
     </>
   )
 }
+
